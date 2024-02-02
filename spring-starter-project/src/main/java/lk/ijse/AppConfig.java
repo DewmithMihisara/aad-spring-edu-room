fdb4d663c2271fcd2c3048aec9948c06d3451fb7
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-02 15:55:23 +0530

    how to get scan specific pkg using base class pages

--- spring-starter-project/src/main/java/lk/ijse/AppConfig.java
+++ spring-starter-project/src/main/java/lk/ijse/AppConfig.java
@@ -1,10 +1,12 @@
 package lk.ijse;
 
+import lk.ijse.bean.SpringBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.bean")
+@ComponentScan(basePackageClasses = SpringBean.class)
+//@ComponentScan(basePackages = "lk.ijse.bean")
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Instantiated");
---
+++ spring-starter-project/src/main/java/lk/ijse/bean/SpringSecondBean.java
@@ -0,0 +1,10 @@
+package lk.ijse.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringSecondBean {
+    public SpringSecondBean() {
+        System.out.println("SpringSecondBean Instantiated");
+    }
+}

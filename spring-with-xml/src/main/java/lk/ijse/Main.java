8dd3a4f701a8202f9da900f407eccdff203b2f37
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-02 15:04:20 +0530

    add xml into ClassPathXmlApplicationContext

--- spring-with-xml/src/main/java/lk/ijse/Main.java
+++ spring-with-xml/src/main/java/lk/ijse/Main.java
@@ -4,6 +4,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
     }
 }

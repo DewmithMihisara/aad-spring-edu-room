061a92d2d40725d3f7efae79fcea5b1f508139a1
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-02 15:43:27 +0530

    how to get scan specific pkg

--- spring-starter-project/src/main/java/lk/ijse/AppConfig.java
+++ spring-starter-project/src/main/java/lk/ijse/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = "lk.ijse.bean")
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Instantiated");

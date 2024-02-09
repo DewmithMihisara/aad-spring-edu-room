d69e026c64714658b2eda4b3195b56e6d62a2efc
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-09 14:42:57 +0530

    make context

---
+++ spring-bean-life-cycle/src/main/java/lk/ijse/AppInitializer.java
@@ -0,0 +1,9 @@
+package lk.ijse;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+    }
+}

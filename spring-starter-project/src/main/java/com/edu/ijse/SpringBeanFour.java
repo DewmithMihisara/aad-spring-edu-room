e4da7f0cf959e7ba5e0b0341fdaaa63d6f29abeb
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-09 13:50:44 +0530

    how to define scope using configurablebeanfactory

--- spring-starter-project/src/main/java/com/edu/ijse/SpringBeanFour.java
+++ spring-starter-project/src/main/java/com/edu/ijse/SpringBeanFour.java
@@ -1,10 +1,12 @@
 package com.edu.ijse;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("four")
-@Scope("prototype")
+//@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class SpringBeanFour {
     public SpringBeanFour() {
         System.out.println("SpringBeanFour Instantiated");

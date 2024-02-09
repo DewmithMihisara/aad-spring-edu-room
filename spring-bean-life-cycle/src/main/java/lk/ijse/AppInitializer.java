e9fd8aaf3d4edc786270508a02bc2a72c0299437
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-09 15:55:26 +0530

    init bean when prototype scope

--- spring-bean-life-cycle/src/main/java/lk/ijse/AppInitializer.java
+++ spring-bean-life-cycle/src/main/java/lk/ijse/AppInitializer.java
@@ -1,5 +1,6 @@
 package lk.ijse;
 
+import lk.ijse.bean.SpringBean;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -7,6 +8,14 @@ public class AppInitializer {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
+
+        SpringBean bean1 = ctx.getBean(SpringBean.class);
+        System.out.println(bean1);
+
+        System.out.println("--------------------------------");
+
+        SpringBean bean2 = ctx.getBean(SpringBean.class);
+        System.out.println(bean2);
 
         ctx.close();
     }
--- spring-bean-life-cycle/src/main/java/lk/ijse/bean/SpringBean.java
+++ spring-bean-life-cycle/src/main/java/lk/ijse/bean/SpringBean.java
@@ -2,11 +2,14 @@ package lk.ijse.bean;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBean(){
         System.out.println("Spring Bean Instantiated....");

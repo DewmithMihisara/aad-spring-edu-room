1e48c48851221885bd1c176b679edea5171b8352
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-09 15:28:19 +0530

    add annotation for init and destroy

--- spring-bean-life-cycle/src/main/java/lk/ijse/bean/SpringBeanTwo.java
+++ spring-bean-life-cycle/src/main/java/lk/ijse/bean/SpringBeanTwo.java
@@ -1,7 +1,17 @@
 package lk.ijse.bean;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanTwo {
+    @PostConstruct
+    public void initialize(){
+        System.out.println("Spring Bean Two Instantiated....");
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Spring Bean Two is Destroyed....");
+    }
 }

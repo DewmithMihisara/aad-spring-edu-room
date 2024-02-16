8eac297816369398ff577b1be4ed974bb050b3c4
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-16 12:38:20 +0530

    dependency ambiguity example with answer

--- dependency-injection/src/main/java/lk/ijse/bean/Sandumi.java
+++ dependency-injection/src/main/java/lk/ijse/bean/Sandumi.java
@@ -1,10 +1,12 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Qualifier("big ass")
 public class Sandumi implements GoodGirl{
     @Override
     public void love() {

bd5d1ce4a94f9917a3db3995eb51e1c72f2a4c93
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 10:03:37 +0530

    add this

--- full-mod-vs-lite-mod/src/main/java/lk/ijse/bean/SpringBeanFour.java
+++ full-mod-vs-lite-mod/src/main/java/lk/ijse/bean/SpringBeanFour.java
@@ -7,31 +7,31 @@ import org.springframework.context.ApplicationContextAware;
 
 public class SpringBeanFour implements BeanFactoryAware, BeanNameAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBeanFour(){
-        System.out.println("SpringBeanFour - Constructor()");
+        System.out.println("SpringBeanFour - Constructor() : "+this);
     }
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("SpringBeanFour - BeanFactory Aware");
+        System.out.println("SpringBeanFour - BeanFactory Aware : "+this);
     }
 
     @Override
     public void setBeanName(String name) {
-        System.out.println("SpringBeanFour - BeanName Aware");
+        System.out.println("SpringBeanFour - BeanName Aware : "+this);
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.println("SpringBeanFour - Disposable Bean");
+        System.out.println("SpringBeanFour - Disposable Bean : "+this);
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("SpringBeanFour - Initializing Bean");
+        System.out.println("SpringBeanFour - Initializing Bean : "+this);
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("SpringBeanFour - ApplicationContext Aware");
+        System.out.println("SpringBeanFour - ApplicationContext Aware : "+this);
     }
 }

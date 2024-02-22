3d3d14f9a1c7037632c568922fc82b74a45b5762
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 15:30:49 +0530

    access os ev using @Value

--- runtime-value-injection/src/main/java/lk/ijse/AppInitializer.java
+++ runtime-value-injection/src/main/java/lk/ijse/AppInitializer.java
@@ -8,28 +8,28 @@ import java.util.Properties;
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-//        context.register(AppConfig.class);
+        context.register(AppConfig.class);
         context.refresh();
         context.registerShutdownHook();
 
-        // access OS environment variables
-        Map<String, String> getenv = System.getenv();
-
-        for (Map.Entry<String, String> entry : getenv.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-
-        System.out.println("===========================");
-
-        // access JRE environment variables
-        Properties properties = System.getProperties();
-
-        // set a new environment variable
-        properties.put("newKey", "newValue");
-
-        for(String key : properties.stringPropertyNames()){
-            System.out.println(key + " : " + properties.get(key));
-        }
+//        // access OS environment variables
+//        Map<String, String> getenv = System.getenv();
+//
+//        for (Map.Entry<String, String> entry : getenv.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
+//
+//        System.out.println("===========================");
+//
+//        // access JRE environment variables
+//        Properties properties = System.getProperties();
+//
+//        // set a new environment variable
+//        properties.put("newKey", "newValue");
+//
+//        for(String key : properties.stringPropertyNames()){
+//            System.out.println(key + " : " + properties.get(key));
+//        }
 
 
     }
--- runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanTwo.java
+++ runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanTwo.java
@@ -1,5 +1,13 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringBeanTwo {
+    public SpringBeanTwo(@Value("${USER}") String user) {
+        System.out.println("SpringBeanTwo : Constructor()");
+        System.out.println("user : " + user);
+    }
 
 }

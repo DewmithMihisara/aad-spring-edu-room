b307974387fddf98083d77703f34961b1c0c7a00
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 14:14:17 +0530

    value example

--- runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanOne.java
+++ runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanOne.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Component
 public class SpringBeanOne {
@@ -14,7 +17,9 @@ public class SpringBeanOne {
                          @Value("5") int no,
                          @Value("5.5") double doubleNO,
                          @Value("true") boolean bool,
-                         @Value("5,6,7,8") int[] arr){
+                         @Value("5,6,7,8") int[] arr,
+                         @Value("5,6,7,8") List<String> list,
+                         @Value("A,B,C,D")Set<String> set){
 
 
         System.out.println("SpringBeanOne - Constructor()");
@@ -24,10 +29,12 @@ public class SpringBeanOne {
         System.out.println("doubleNO : "+doubleNO);
         System.out.println("bool : "+bool);
         System.out.println("arr : "+ Arrays.toString(arr));
+        System.out.println("list : "+list);
+        System.out.println("set : " + set);
     }
 
     @PostConstruct
     private void initialize(){
-        System.out.println("name : "+name);
+//        System.out.println("name : "+name);
     }
 }

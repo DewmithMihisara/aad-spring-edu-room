4172b4885da0e872865d5c8db7b3928c224e797a
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 14:04:21 +0530

    value example

--- runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanOne.java
+++ runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanOne.java
@@ -4,14 +4,28 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class SpringBeanOne {
     @Value("Chamath")
     private String name;
-    public SpringBeanOne(@Value("puka") String myString){
-        System.out.println("SpringBeanOne - Constructor() : " + myString);
-        System.out.println("name : "+name);
+    public SpringBeanOne(@Value("test Name") String string,
+                         @Value("5") int no,
+                         @Value("5.5") double doubleNO,
+                         @Value("true") boolean bool,
+                         @Value("5,6,7,8") int[] arr){
+
+
+        System.out.println("SpringBeanOne - Constructor()");
+
+        System.out.println("string : "+string);
+        System.out.println("no : "+no);
+        System.out.println("doubleNO : "+doubleNO);
+        System.out.println("bool : "+bool);
+        System.out.println("arr : "+ Arrays.toString(arr));
     }
+
     @PostConstruct
     private void initialize(){
         System.out.println("name : "+name);

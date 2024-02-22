6c7fd49de2ed4f92295d00977a160ab07d75cac1
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 16:19:59 +0530

    use Environment interface

--- runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanThree.java
+++ runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanThree.java
@@ -1,10 +1,24 @@
 package lk.ijse.bean;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanThree {
+    @Autowired
+    private Environment env;
     public SpringBeanThree() {
         System.out.println("SpringBeanThree : Constructor()");
+    }
+    @PostConstruct
+    private void initialize() {
+        System.out.println(env.getProperty("app.env1"));
+        System.out.println(env.getProperty("app.env2"));
+        System.out.println(env.getProperty("app.env3"));
+        System.out.println(env.getProperty("app.env4"));
+        System.out.println(env.getProperty("os.name"));
+        System.out.println(env.getProperty("USER"));
     }
 }

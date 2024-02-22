9652d8adf89b4993be36a694c319f6dbdc6edf1b
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 09:38:55 +0530

    add comment

--- full-mod-vs-lite-mod/src/main/java/lk/ijse/bean/SpringBeanOne.java
+++ full-mod-vs-lite-mod/src/main/java/lk/ijse/bean/SpringBeanOne.java
@@ -2,12 +2,15 @@ package lk.ijse.bean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
 //SpringBeanOne in full mode
 @Component
 public class SpringBeanOne {
     public SpringBeanOne(){
         System.out.println("SpringBeanOne - Constructor()");
     }
+
+    //SpringBeanThree in lite mode
     @Bean
     public SpringBeanThree springBeanThree(){
         return new SpringBeanThree();

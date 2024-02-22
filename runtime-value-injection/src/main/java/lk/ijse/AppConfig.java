8e96a9ee81badb3100f053a5e610be29ebab260b
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 15:56:01 +0530

    @PropertySource usage

--- runtime-value-injection/src/main/java/lk/ijse/AppConfig.java
+++ runtime-value-injection/src/main/java/lk/ijse/AppConfig.java
@@ -2,8 +2,10 @@ package lk.ijse;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 }
--- runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanTwo.java
+++ runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanTwo.java
@@ -1,11 +1,25 @@
 package lk.ijse.bean;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanTwo {
+    @Value("${app.env1}")
+    private String env1;
+
+    @Value("${app.env2}")
+    private String env2;
+
+    @Value("${app.env3}")
+    private String env3;
+
+    @Value("${app.env4}")
+    private int env4;
+
     //how to access os ev
+
 //    public SpringBeanTwo(@Value("${USER}") String user) {
 //        System.out.println("SpringBeanTwo : Constructor()");
 //        System.out.println("user : " + user);
@@ -17,4 +31,11 @@ public class SpringBeanTwo {
         System.out.println("osName : " + osName);
     }
 
+    @PostConstruct
+    private void initialize() {
+        System.out.println("env1 : " + env1);
+        System.out.println("env2 : " + env2);
+        System.out.println("env3 : " + env3);
+        System.out.println("env4 : " + env4);
+    }
 }

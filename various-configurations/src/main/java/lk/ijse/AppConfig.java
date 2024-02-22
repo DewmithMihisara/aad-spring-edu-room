fe323ed7734f779fcc6ee029e5accf6e32a91e1a
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 12:03:40 +0530

    @ComponentScan

--- various-configurations/src/main/java/lk/ijse/AppConfig.java
+++ various-configurations/src/main/java/lk/ijse/AppConfig.java
@@ -1,11 +1,12 @@
 package lk.ijse;
 
+import lk.ijse.bean.AnnotationConfigSpringBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ComponentScan
+@ComponentScan("lk.ijse.bean")
 @ImportResource("classpath:context.xml")
 public class AppConfig {
 }
--- various-configurations/src/main/java/lk/ijse/DAOConfig.java
+++ various-configurations/src/main/java/lk/ijse/DAOConfig.java
@@ -1,7 +1,9 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("lk.ijse.dao")
 public class DAOConfig {
 }

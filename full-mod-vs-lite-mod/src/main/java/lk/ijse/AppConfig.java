4920b120dddff7747cfdee75d8f6ea7c86a428ce
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 09:35:07 +0530

    define implicitly SpringBeanTwo

--- full-mod-vs-lite-mod/src/main/java/lk/ijse/AppConfig.java
+++ full-mod-vs-lite-mod/src/main/java/lk/ijse/AppConfig.java
@@ -1,9 +1,15 @@
 package lk.ijse;
 
+import lk.ijse.bean.SpringBeanTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
+    @Bean
+    public SpringBeanTwo springBeanTwo(){
+        return new SpringBeanTwo();
+    }
 }

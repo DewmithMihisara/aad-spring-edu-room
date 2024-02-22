c33a46d51a36d6ec7ff0a937892fe4c240bcf98a
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 11:41:07 +0530

    @ImportResource

--- various-configurations/src/main/java/lk/ijse/AppConfig.java
+++ various-configurations/src/main/java/lk/ijse/AppConfig.java
@@ -2,8 +2,10 @@ package lk.ijse;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan
+@ImportResource("classpath:context.xml")
 public class AppConfig {
 }

13dbc3e1d95c27110170109e31ac922f6250bf1e
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 11:37:09 +0530

    add annotation base bean

---
+++ various-configurations/src/main/java/lk/ijse/bean/AnnotationConfigSpringBean.java
@@ -0,0 +1,10 @@
+package lk.ijse.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnnotationConfigSpringBean {
+    public AnnotationConfigSpringBean() {
+        System.out.println("AnnotationConfigSpringBean - constructor()");
+    }
+}

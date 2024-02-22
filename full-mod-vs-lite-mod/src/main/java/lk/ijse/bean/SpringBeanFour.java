66e6e536669035196934cdb4ebaead3f1d9a3866
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 09:56:39 +0530

    add interfaces for watch life cycle of SpringBeanFour

--- full-mod-vs-lite-mod/src/main/java/lk/ijse/bean/SpringBeanFour.java
+++ full-mod-vs-lite-mod/src/main/java/lk/ijse/bean/SpringBeanFour.java
@@ -1,7 +1,37 @@
 package lk.ijse.bean;
 
-public class SpringBeanFour {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class SpringBeanFour implements BeanFactoryAware, BeanNameAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBeanFour(){
         System.out.println("SpringBeanFour - Constructor()");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("SpringBeanFour - BeanFactory Aware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("SpringBeanFour - BeanName Aware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("SpringBeanFour - Disposable Bean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("SpringBeanFour - Initializing Bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("SpringBeanFour - ApplicationContext Aware");
     }
 }

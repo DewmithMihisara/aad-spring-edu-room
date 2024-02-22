bb3f0f8458e9c6c7b0f753a4fbc31d978446b964
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-02-22 12:56:27 +0530

    @Value into constructor

--- runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanOne.java
+++ runtime-value-injection/src/main/java/lk/ijse/bean/SpringBeanOne.java
@@ -1,10 +1,11 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanOne {
-    public SpringBeanOne(String myString){
+    public SpringBeanOne(@Value("puka") String myString){
         System.out.println("SpringBeanOne - Constructor() : " + myString);
     }
 }

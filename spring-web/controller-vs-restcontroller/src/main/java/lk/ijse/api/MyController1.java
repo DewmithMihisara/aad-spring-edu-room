7f7816999e43f6c2cba624f729a7fb0ca083cef5
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 15:34:00 +0530

    add constructors into api

---
+++ spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/api/MyController1.java
@@ -0,0 +1,10 @@
+package lk.ijse.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController1 {
+    public MyController1() {
+        System.out.println("MyController1 Instantiated");
+    }
+}

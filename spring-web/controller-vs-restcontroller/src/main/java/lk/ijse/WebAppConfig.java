2e82a85eff02ba8d0b5892c883a8e973c0b147b6
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 16:36:59 +0530

    use controller

--- spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/WebAppConfig.java
+++ spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/WebAppConfig.java
@@ -4,13 +4,25 @@ import lk.ijse.api.MyController1;
 import lk.ijse.api.MyController2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig() {
         System.out.println("WebAppConfig Instantiated -constructor()");
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/WEB-INF/pages/**").addResourceLocations("/WEB-INF/WEB-INF/pages/");
+    }
+    @Bean
+    public ViewResolver viewResolver(){
+        return new InternalResourceViewResolver("/WEB-INF/WEB-INF/pages/", ".html");
     }
     @Bean
     public MyController1 myController1(){
--- spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/api/MyController2.java
+++ spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/api/MyController2.java
@@ -12,6 +12,6 @@ public class MyController2 {
     }
     @GetMapping
     public String hello(){
-        return "Hello from MyController2";
+        return "someThing";
     }
 }

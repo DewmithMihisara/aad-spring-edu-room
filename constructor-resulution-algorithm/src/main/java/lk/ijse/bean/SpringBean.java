e9854e3d9d0e8668c4ad5ac8e690d0ccd8f38e80
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 09:01:11 +0530

    add instance

--- constructor-resulution-algorithm/src/main/java/lk/ijse/bean/SpringBean.java
+++ constructor-resulution-algorithm/src/main/java/lk/ijse/bean/SpringBean.java
@@ -4,7 +4,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
+    private String name;
+    private int age;
+    private boolean isMale;
+    private float temp;
+
     public SpringBean() {
         System.out.println("SpringBean - Constructor");
     }
+
+
+
 }

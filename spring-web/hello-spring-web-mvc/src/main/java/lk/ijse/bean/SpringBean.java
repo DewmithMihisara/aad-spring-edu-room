ba01770f430f07250ce0430fb81efed46b623d8f
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 11:02:06 +0530

    implement interface into bean for view life cycle

--- spring-web/hello-spring-web-mvc/src/main/java/lk/ijse/bean/SpringBean.java
+++ spring-web/hello-spring-web-mvc/src/main/java/lk/ijse/bean/SpringBean.java
@@ -1,10 +1,39 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public SpringBean() {
         System.out.println("Spring bean-constructor()");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
     }
 }

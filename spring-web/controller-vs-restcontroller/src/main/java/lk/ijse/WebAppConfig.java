9176815751191c84aa6bbf94ab729b436f5bc3b8
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 15:50:53 +0530

    change some

--- spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/WebAppConfig.java
+++ spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/WebAppConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse;
 
+import lk.ijse.api.MyController1;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -8,5 +10,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig Instantiated -constructor()");
+    }
+    @Bean
+    public MyController1 myController1(){
+        return new MyController1();
     }
 }

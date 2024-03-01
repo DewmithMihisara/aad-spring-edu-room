3a43772dd0db909d029285a1d3f1d084f3eaf2b3
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 15:54:54 +0530

    add handler into controller 1

--- spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/api/MyController1.java
+++ spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/api/MyController1.java
@@ -1,10 +1,17 @@
 package lk.ijse.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/one")
 public class MyController1 {
     public MyController1() {
         System.out.println("MyController1 Instantiated -constructor()");
+    }
+    @GetMapping
+    public String hello(){
+        return "Hello from MyController1";
     }
 }

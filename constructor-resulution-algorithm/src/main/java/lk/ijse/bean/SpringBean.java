83b5f2f85a69923e6fbcd0f7a1f2f4c6cb951e59
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 09:05:16 +0530

    add constructor with nullable

--- constructor-resulution-algorithm/src/main/java/lk/ijse/bean/SpringBean.java
+++ constructor-resulution-algorithm/src/main/java/lk/ijse/bean/SpringBean.java
@@ -1,5 +1,6 @@
 package lk.ijse.bean;
 
+import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,16 +10,24 @@ public class SpringBean {
     private boolean isMale;
     private float temp;
 
-//    public SpringBean() {
+    //    public SpringBean() {
 //        System.out.println("SpringBean - Constructor");
 //    }
-    public SpringBean(String name, int age, boolean isMale) {
+
+//    public SpringBean(String name, int age, boolean isMale) {
+//        this.name = name;
+//        this.age = age;
+//        this.isMale = isMale;
+//        System.out.println("Spring Bean :(String,int,boolean)");
+//        System.out.printf("name=%s, age=%s, isMale=%s\n", name, age, isMale);
+//    }
+    public SpringBean(String name, int age, boolean isMale, @Nullable float temp) {
         this.name = name;
         this.age = age;
         this.isMale = isMale;
-        System.out.println("Spring Bean :(String,int,boolean)");
-        System.out.printf("name=%s, age=%s, isMale=%s\n", name, age, isMale);
+        this.temp = temp;
+        System.out.println("Spring Bean :(String,int,boolean,float)");
+        System.out.printf("name=%s, age=%s, isMale=%s, temp=%s\n", name, age, isMale, temp);
     }
-
 
 }

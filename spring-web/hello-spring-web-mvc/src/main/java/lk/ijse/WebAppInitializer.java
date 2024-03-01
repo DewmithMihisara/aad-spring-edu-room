9865264fc08089ca9d0c58cc747fbc394dbe1d41
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 10:38:20 +0530

    extend web app initializer by AbstractAnnotationConfigDispatcherServletInitializer

--- spring-web/hello-spring-web-mvc/src/main/java/lk/ijse/WebAppInitializer.java
+++ spring-web/hello-spring-web-mvc/src/main/java/lk/ijse/WebAppInitializer.java
@@ -1,7 +1,21 @@
 package lk.ijse;
 
-public class WebAppInitializer {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
     }
 }

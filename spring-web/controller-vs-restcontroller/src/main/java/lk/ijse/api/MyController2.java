49ad384fe0373d21857ef62b2167e88744364871
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 15:51:11 +0530

    add another class

---
+++ spring-web/controller-vs-restcontroller/src/main/java/lk/ijse/api/MyController2.java
@@ -0,0 +1,10 @@
+package lk.ijse.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController2 {
+    public MyController2() {
+        System.out.println("MyController2 Instantiated -constructor()");
+    }
+}

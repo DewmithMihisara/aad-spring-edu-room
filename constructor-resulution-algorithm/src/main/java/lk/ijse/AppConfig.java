d18475f0a227c918c14ffbae70aa7aea4f8fc8f5
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-01 08:52:37 +0530

    add string and int into application context

--- constructor-resulution-algorithm/src/main/java/lk/ijse/AppConfig.java
+++ constructor-resulution-algorithm/src/main/java/lk/ijse/AppConfig.java
@@ -1,9 +1,18 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
+    @Bean
+    public String myName(){
+        return "Perera";
+    }
+    @Bean
+    public Integer myInt(){
+        return 31;
+    }
 }

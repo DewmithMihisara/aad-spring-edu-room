6414e509190e3d59e71f412375edfa74fbe74879
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 10:04:46 +0530

    add mapping specification project

--- spring-web/simple-web-crud-example/src/main/java/lk/ijse/WebAppConfig.java
+++ spring-web/simple-web-crud-example/src/main/java/lk/ijse/WebAppConfig.java
@@ -1,9 +1,11 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
+@ComponentScan
 @EnableWebMvc
 public class WebAppConfig implements WebMvcConfigurer {
 }
--- spring-web/simple-web-crud-example/src/main/java/lk/ijse/WebRootConfig.java
+++ spring-web/simple-web-crud-example/src/main/java/lk/ijse/WebRootConfig.java
@@ -1,7 +1,9 @@
 package lk.ijse;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:hibernate.properties")
 public class WebRootConfig {
 }

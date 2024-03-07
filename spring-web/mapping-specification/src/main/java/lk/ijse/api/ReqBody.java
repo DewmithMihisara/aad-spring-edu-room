185567e857b447831db4e2d8480b6baf5254bc8b
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 15:02:48 +0530

    req body done

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/ReqBody.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/ReqBody.java
@@ -1,7 +1,21 @@
 package lk.ijse.api;
 
+import lk.ijse.entity.Student;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReqBody {
+    @PostMapping("/svCus")
+    public String svCus(@RequestBody String id, @RequestBody String name, @RequestBody String age){
+        return String.format("id:%s  |  name:%s  |  age:%s",id,name,age);
+    }
+
+    @PostMapping(value = "/svStd")
+    public String svStd(@ModelAttribute Student std){
+        return std.toString();
+    }
 }

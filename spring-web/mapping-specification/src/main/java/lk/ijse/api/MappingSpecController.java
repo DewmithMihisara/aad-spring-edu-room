64ba182a0b59dedf7b17e2d9c83ffe61d8e25456
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 10:28:16 +0530

    narrow down req using path

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/MappingSpecController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/MappingSpecController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/map")
 public class MappingSpecController {
-
     @GetMapping
-    public String getMap(){
-        return "getMapMethod()";
+    public String getMapOne(){
+        return "getMapOne()";
+    }
+    @GetMapping(path = "/ijse")
+    public String getMapTwo(){
+        return "getMapTwo()";
     }
 }

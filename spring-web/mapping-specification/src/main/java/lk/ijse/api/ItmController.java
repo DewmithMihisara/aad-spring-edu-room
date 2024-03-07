87ca2904a8d97b3cb891bc020608c9ff8894186e
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 16:11:09 +0530

    test

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/ItmController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/ItmController.java
@@ -1,7 +1,23 @@
 package lk.ijse.api;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/itm")
 public class ItmController {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String svItmJson(){
+        return "I accept only application/json";
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String svItmXml(){
+        return "I accept only application/xml";
+    }
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String svItmMultiForm(){
+        return "I accept only multipart/form";
+    }
 }

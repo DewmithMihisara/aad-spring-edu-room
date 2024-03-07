26e949fa248a0c2ce5edcd7f58034602aff394f5
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 13:04:13 +0530

    work with query params

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/SupplierController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/SupplierController.java
@@ -2,14 +2,19 @@ package lk.ijse.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sup")
 public class SupplierController {
     @GetMapping
-    public String getSuppliers(){
-        return "getSuppliers()";
+    public String getSuppliers(@RequestParam("id") String id){
+        return String.format("id : %s",id);
     }
 
+    @GetMapping("/getag")
+    public String getAge(@RequestParam("id") String id, @RequestParam("age") int age){
+        return String.format("id : %s  |  age : %s",id,age);
+    }
 }

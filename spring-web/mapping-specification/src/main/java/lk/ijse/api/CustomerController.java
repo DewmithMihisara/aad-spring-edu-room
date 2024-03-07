167147645e51221edfad2646cff84317a68030d0
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 11:36:11 +0530

    add handler cus controller

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/CustomerController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/CustomerController.java
@@ -1,7 +1,15 @@
 package lk.ijse.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cus")
 public class CustomerController {
+    @GetMapping
+    public String getCus(){
+        return "gat cus";
+    }
+
 }

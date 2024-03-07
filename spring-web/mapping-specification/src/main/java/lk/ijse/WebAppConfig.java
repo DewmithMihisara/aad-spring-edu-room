f1de10d2a1193631ad84353034a9d389e683c311
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 10:19:46 +0530

    set handler

--- spring-web/mapping-specification/src/main/java/lk/ijse/WebAppConfig.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/WebAppConfig.java
@@ -1,9 +1,11 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class WebAppConfig implements WebMvcConfigurer {
 }
--- spring-web/mapping-specification/src/main/java/lk/ijse/api/MappingSpecController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/MappingSpecController.java
@@ -1,9 +1,11 @@
 package lk.ijse.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/map")
+@RestController
+@RequestMapping("/map")
 public class MappingSpecController {
 
     @GetMapping

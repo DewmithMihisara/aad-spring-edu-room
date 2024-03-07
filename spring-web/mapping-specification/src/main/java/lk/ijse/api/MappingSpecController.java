a26d8e3906d850ac3c39ddc3d36238432a8ac446
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 10:14:14 +0530

    add handler

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/MappingSpecController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/MappingSpecController.java
@@ -1,7 +1,13 @@
 package lk.ijse.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/map")
 public class MappingSpecController {
+
+    @GetMapping
+    public String getMap(){
+        return "getMapMethod()";
+    }
 }

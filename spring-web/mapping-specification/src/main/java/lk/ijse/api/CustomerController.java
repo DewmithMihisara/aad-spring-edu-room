301b6bd109d403774573b635f32822bd35440c46
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 11:58:43 +0530

    use path variables

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/CustomerController.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/CustomerController.java
@@ -1,15 +1,28 @@
 package lk.ijse.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cus")
 public class CustomerController {
+
     @GetMapping
     public String getCus(){
         return "gat cus";
+    }
+
+
+    @GetMapping("/{city}")
+    public String getCusCity(@PathVariable("city") String city){
+        return String.format("getCus(String city):%s",city);
+    }
+
+    @GetMapping("/{city}/{name}")
+    public String getCusCityAndName(@PathVariable("city") String city, @PathVariable String name){
+        return String.format("city : %s   |   name : %s",city,name);
     }
 
 }

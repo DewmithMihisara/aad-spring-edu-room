28e58e4261fb11d7b6c8f97a4b9177073671a9df
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 15:34:26 +0530

    add jackson

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/WithJson.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/WithJson.java
@@ -1,7 +1,28 @@
 package lk.ijse.api;
 
+import lk.ijse.entity.Student;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WithJson {
+
+//    @PostMapping("/jsn")
+//    public String sv( String id,  String name,  String age) {
+//        return String.format("id:%s   |   name:%s     |   age:%s", id, name, age);
+//    }
+
+    @PostMapping("/jsn")
+    public String sv(@RequestBody Student std) {
+        return std.toString();
+    }
+
+    @GetMapping(value = "/gt", produces = "application/json")
+    public Student gt() {
+//        return ResponseEntity.status(200).body(new Student("c001","amal",20));
+        return new Student("c001","amal",20);
+    }
 }
--- spring-web/mapping-specification/src/main/java/lk/ijse/entity/Student.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/entity/Student.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 public class Student {
     private String id;
     private String name;
-    private String age;
+    private int age;
 }

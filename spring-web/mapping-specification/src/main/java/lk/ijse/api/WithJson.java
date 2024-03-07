07f4c3aa76f710ecd74c7de1889078c9c186d677
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-07 15:50:58 +0530

    serialize data

--- spring-web/mapping-specification/src/main/java/lk/ijse/api/WithJson.java
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/WithJson.java
@@ -1,11 +1,14 @@
 package lk.ijse.api;
 
 import lk.ijse.entity.Student;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class WithJson {
@@ -22,7 +25,15 @@ public class WithJson {
 
     @GetMapping(value = "/gt", produces = "application/json")
     public Student gt() {
-//        return ResponseEntity.status(200).body(new Student("c001","amal",20));
         return new Student("c001","amal",20);
+    }
+
+    @GetMapping(value = "gtAll",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<Student>gtAll(){
+        ArrayList<Student>st=new ArrayList<>();
+        st.add(new Student("st001","amal",20));
+        st.add(new Student("st002","nimal",30));
+        st.add(new Student("st003","kamal",40));
+        return st;
     }
 }

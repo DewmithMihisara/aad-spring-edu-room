472467d7eb1d35cf42859489e048d8092d641f2b
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-14 15:46:39 +0530

    add testing api

---
+++ spring-web/spring-web-project/src/main/java/lk/ijse/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package lk.ijse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-03-14
+ * @since 1.0.0
+ */
+@RestController
+public class CustomerController {
+    @GetMapping("/customers")
+    public String getAllCustomer(){
+        return "Hello from CustomerController";
+    }
+}

493f940ee3ea179f1e1bb5664d92de7f95668256
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-14 15:56:29 +0530

    done cus dto

--- spring-web/spring-web-project/src/main/java/lk/ijse/dto/CustomerDTO.java
+++ spring-web/spring-web-project/src/main/java/lk/ijse/dto/CustomerDTO.java
@@ -1,9 +1,19 @@
 package lk.ijse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Dewmith Mihisara
  * @date 2024-03-14
  * @since 1.0.0
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
+    private String id;
+    private String name;
+    private String address;
 }

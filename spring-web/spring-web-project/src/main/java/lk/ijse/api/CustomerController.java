4cc5d72e1f509524ab5deb9199aa4259a5ecb2ec
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-14 15:55:32 +0530

    add cus dto

--- spring-web/spring-web-project/src/main/java/lk/ijse/api/CustomerController.java
+++ spring-web/spring-web-project/src/main/java/lk/ijse/api/CustomerController.java
@@ -1,4 +1,4 @@
-package lk.ijse.controller;
+package lk.ijse.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ spring-web/spring-web-project/src/main/java/lk/ijse/dto/CustomerDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.dto;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-03-14
+ * @since 1.0.0
+ */
+public class CustomerDTO {
+}

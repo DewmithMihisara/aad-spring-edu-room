0c940f2fadbe506f545b264450f9ac3ae70232ad
Dewmith Mihisara <mihisaralokuhewage@gmail.com>
2024-03-14 09:08:44 +0530

    make order controller

---
+++ spring-web/mapping-specification/src/main/java/lk/ijse/api/OrderController.java
@@ -0,0 +1,15 @@
+package lk.ijse.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-03-14
+ * @since 0.1.0
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+}

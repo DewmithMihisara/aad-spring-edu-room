a1e6dd6c201e5a37b313023402b6523640eb6bb2
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-03-28 05:24:12 +0530

    add project

---
+++ spring-web-project/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,12 @@
+package lk.ijse;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-03-28
+ * @since ${VERSION}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

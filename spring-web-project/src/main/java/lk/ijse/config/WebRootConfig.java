224d67a4f75630e238ae1bc8a9f3cac5fb08a773
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-03-28 05:49:15 +0530

    add webb root config

---
+++ spring-web-project/src/main/java/lk/ijse/config/WebRootConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-03-28
+ * @since 1.0.0
+ */
+@Configuration
+public class WebRootConfig {
+}

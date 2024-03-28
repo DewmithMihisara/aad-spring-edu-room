00ca9accc3cd68e6dff02ba3e5674a4df9e899aa
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-03-28 05:49:09 +0530

    add webb app config

---
+++ spring-web-project/src/main/java/lk/ijse/config/WebAppConfig.java
@@ -0,0 +1,16 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author Dewmith Mihisara
+ * @date 2024-03-28
+ * @since 1.0.0
+ */
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebAppConfig {
+}
